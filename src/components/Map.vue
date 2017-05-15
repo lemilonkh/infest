623ace4ce79150e15a1c3ee05ef79a1384d245af
Milan Gruner <milangruner@gmail.com>
2017-05-15 16:55:53 +0200

    :poop: Adds non-finished Map.vue component
    Needs more code from https://www.npmjs.com/package/vue2-leaflet

---
+++ src/components/Map.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-map :zoom=13 :center="[47.413220, -1.219482]">
+    <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+    <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
+  </v-map>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus">
+  @import "~leaflet/dist/leaflet.css";
+</style>
