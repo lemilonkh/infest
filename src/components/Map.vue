a11afccb827962c62d5310575f342b8a1c585c07
Milan Gruner <milangruner@gmail.com>
2017-05-16 15:02:23 +0200

    :sparkles: Adds Map component (untested)

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,14 +1,34 @@
-<template>
-  <v-map :zoom=13 :center="[47.413220, -1.219482]">
-    <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
-    <v-marker :lat-lng="[47.413220, -1.219482]"></v-marker>
-  </v-map>
+<template lang="jade">
+div style="height: 100%"
+  p {{ greeting }} World!
+  v-map :zoom=13 :center="[47.413220, -1.219482]"
+    v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+    v-marker :lat-lng="[47.413220, -1.219482]"
 </template>
 
 <script>
+import Vue2Leaflet from 'vue2-leaflet';
 
+const Map = {
+  data() {
+    return {
+      greeting: 'Hello'
+    }
+  },
+  components: {
+    'v-map': Vue2Leaflet.Map,
+    'v-tilelayer': Vue2Leaflet.TileLayer,
+    'v-marker': Vue2Leaflet.Marker
+  }
+};
+
+export default Map;
 </script>
 
 <style lang="stylus">
-  @import "~leaflet/dist/leaflet.css";
+@import "~leaflet/dist/leaflet.css";
+
+p
+  font-size 2em
+  text-align center
 </style>
